948d9f0290980c7268f8364fd88b8110f58620b8
rorororom <mariia.romashkina.mih@gmail.com>
2023-08-22 13:38:58 +0300

    структур

--- popytochka.c
+++ popytochka.c
@@ -10,56 +10,65 @@ enum root{
     INFINITY_ROOTS
 };
 
+struct coefficient{
+    float a;
+    float b;
+    float c;
+};
 
-int comparison0(int a); // проверка числа на равенство 0
+
+int comparison0(int a);
 int solve_the_equation(float a, float b, float c, float *x1, float *x2);
-int solve_linear_r_the_equation(float b, float c, float *x1);
+int solve_linear_the_equation(float b, float c, float *x1);
 
 int main()
 {
     float a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
-    if ((scanf("%f%f%f", &a, &b, &c)) != 3) // проверка на правильность ввода
+    if ((scanf("%f%f%f", &a, &b, &c)) != 3)
         printf("Неверное введенное значение");
-    else // если корректный ввод, то переходит к решению уравнения
+    else
     {
-        int number_of_roots = solve_the_equation(a,b,c, &x1, &x2); // запускаем функцию, считающую кол-во корней
-        switch (number_of_roots)
+        printf("kek0\n");
+        int nRoots = solve_the_equation(a,b,c, &x1, &x2);
+        switch (nRoots)
         {
-            case TWO_ROOTS:
-                printf("x1 = %f, x2 = %f\n", x1, x2); //если 2 корня
+            case TWO_ROOTS: printf("x1 = %f, x2 = %f\n", x1, x2);
                 break;
-            case ONE_ROOTS:
-                printf("x1 = %f\n", x1); // если 1 корень
+            case ONE_ROOTS: printf("x = %f\n", x1);
                 break;
-            case NO_ROOTS:
-                printf("корней нет"); // если 0 корней
+            case NO_ROOTS: printf("корней нет");
                 break;
-            default:
-                printf("бесконечное кол-во корней");
+            case INFINITY_ROOTS: printf("бесконечное кол-во корней");
                 break;
+            default: printf("");
         }
     }
 }
 
 
-int comparison0(int a)
+int comparison0(float a)
 {
-    if (abs(a - 0) < EPS) return 1;// если число равно 0, то выводим истину return 1;
-    return 0; // иначе ложь
+    if (abs(a - 0) < EPS) return 1;
+    return 0;
 }
 
 
 int solve_the_equation(float a, float b, float c, float *x1, float *x2)
 {
-    if (comparison0(a) == 0) // если а не 0
+    assert(x1 != NULL);
+    assert(x2 != NULL);
+    printf("kek1\n");
+    if (comparison0(a) == 0)
     {
-        float discriminant = b * b - 4 * a * c; // считаем дискриминант;
-        if (comparison0(discriminant) == 1) // если дискриминант 0
+        printf("kek2\n");
+        float discriminant = b * b - 4 * a * c;
+        if (comparison0(discriminant) == 1)
         {
+            printf("sg\n");
             *x1 = -b / (2 * a);
             return ONE_ROOTS;
         }
-        else if (discriminant > 0) // если дискриминант больше 0
+        else if (discriminant > 0)
         {
             *x1 = (-b + sqrt(discriminant)) / (2 * a);
             *x2 = (-b - sqrt(discriminant)) / (2 * a);
@@ -67,11 +76,11 @@ int solve_the_equation(float a, float b, float c, float *x1, float *x2)
         }
         return NO_ROOTS;
     }
-    else return solve_linear_r_the_equation(b, c, x1); // случай, если а = 0
+    else return solve_linear_the_equation(b, c, x1);
 }
 
 
-int solve_linear_r_the_equation(float b, float c, float *x1)
+int solve_linear_the_equation(float b, float c, float *x1)
 {
     if (comparison0(b) == 1 && comparison0(c) == 1) return INFINITY_ROOTS;
     else if(comparison0(b) == 1) return NO_ROOTS;
@@ -80,5 +89,6 @@ int solve_linear_r_the_equation(float b, float c, float *x1)
         *x1 = -c / b;
         return ONE_ROOTS;
     }
+
 }
 
